5ed8d60ed75b423c93e2c7d83531da4c83c46949
dtbuild <dtbuild@rigil>
2026-09-25 11:22:04 +0000

    fcc1573818f29c93c820fe7dbfd55660435acefe Release 3.1.1
    Sync to source repo @fcc1573818f29c93c820fe7dbfd55660435acefe

--- js/keyTable.bootstrap5.min.mjs
+++ js/keyTable.bootstrap5.min.mjs
@@ -1,4 +1,4 @@
-/*! KeyTable Bootstrap 5 styling 3.1.0 for DataTables
+/*! KeyTable Bootstrap 5 styling 3.1.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bs5";import KeyTable from"datatables.net-keytable";export default DataTable;
